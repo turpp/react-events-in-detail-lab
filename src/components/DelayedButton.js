81f25a87622c1cad74e190607a368ed1aa6a370f
turpp <white.tripp@icloud.com>
2021-02-06 17:38:02 -0600

    commit tree 4de6b62b7735900cd0812dd328eecfa51bbd66ba

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,27 @@
 // Code DelayedButton Component Here
+import React from 'react'
+
+export default class DelayedButton extends React.Component{
+    callfn =(event) => {
+        event.persist()
+        return (event) => setTimeout(this.props.onDelayedClick, this.props.delay)
+        // return this.props.onDelayedClick
+    } 
+
+    somefn = (event) => {
+        event.persist()
+        return ()=> this.props.onDelayedClick(event)
+    }
+    render(){
+        
+        // return <button onClick={(event)=>setTimeout(this.props.onDelayedClick, this.props.delay)} type='button' />
+        // return <button onClick={this.callfn} type='button' />
+        // return <button onClick={event=>setTimeout(this.callfn(event), this.props.delay)} type='button' />
+        // return <button onClick={event => setTimeout(()=>this.props.onDelayedClick(event), this.props.delay)}/>
+        return <button onClick={event => setTimeout(this.somefn(event), this.props.delay)}/>
+
+    }
+
+
+
+}
